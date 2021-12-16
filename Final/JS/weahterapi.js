2d0f454f6cf0476170a5b05d81401043f8fdb471
Shaileyru <kik18001@byui.edu>
2021-12-15 22:41:20 -0800

    update

--- Final/JS/weahterapi.js
+++ Final/JS/weahterapi.js
@@ -10,3 +10,48 @@ fetch(apiURL)
         document.getElementById('humid').textContent = humid;
 
     });
+    
+const weatherURL = "https://api.openweathermap.org/data/2.5/weather?id=3530103&appid=8dd97b200c8afe4ed2b4724eaee03d91&lang=en&units=imperial"
+const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=3530103&appid=8dd97b200c8afe4ed2b4724eaee03d91&lang=en&units=imperial"
+
+fetch(weatherURL) .then(response => response.json())
+.then(data => {
+    document.getElementById('temp').innerText = Math.round(data.main.temp);
+    document.getElementById('high').innerText = Math.round(data.main.temp_max);
+    document.getElementById('windSpeed').innerText = data.wind.speed;
+    document.getElementById('humidity').innerText = data.main.humidity;
+    let currentCondition = data.weather[0].description
+    document.getElementById('condition').innerText = currentCondition.charAt(0).toUpperCase() + currentCondition.substring(1);
+    })
+
+fetch(forecastURL) .then(response => response.json())
+.then(data => {
+    newDate = '';
+    weekTemp = {};
+    days = [];
+
+    weekTemp = Object.assign(...data.list
+        .filter(el => el.dt_txt.indexOf('18:00:00') >= 0)
+        .map((el, index) => {
+            console.log(el)
+                newDate = new Date(el.dt_txt)
+
+                console.log(newDate.toString().substr(0,3))
+
+                return({
+                    [newDate.toString().substr(0,3)] : Math.round(el.main.temp) 
+                })
+            }       
+        ));
+
+        console.log(weekTemp)
+
+        let properties =  Object.keys(weekTemp)
+        let values =  Object.values(weekTemp)
+        console.log(properties, values)
+        for(i=0; i< properties.length; i++){
+            console.log("dayName" + (i + 1))
+            document.getElementById("dayName" + (i + 1)).innerText = properties[i];
+            document.getElementById("dayTemp" + (i + 1)).innerText = values[i] + " °F";
+        }
+    })
